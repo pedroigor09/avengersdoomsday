e4904cc513ac3148a216cee1c8457f188cf8e2ef
pedroigor09 <peuigor09@gmail.com>
2025-12-24 14:37:35 -0300

    feat: update metadata icons to use SVG data URL for improved rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export const metadata: Metadata = {
   title: "Countdown To Doomsday",
   description: "Avengers: Doomsday - December 18, 2026",
   icons: {
-    icon: "/logo.png",
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">❇️</text></svg>',
+      },
+    ],
   },
 };
 
